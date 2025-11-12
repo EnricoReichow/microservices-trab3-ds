7bfee0087042fd6a81bc14679fe791e00350d964
Enrico Bernz Reichow <enricobernzreichow@gmail.com>
2025-11-12 20:35:34 -0300

    novo serviço cru

---
+++ src/main/java/recepcionista-backend/src/main/java/com/example/recepcionista_backend/RecepcionistaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.recepcionista_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecepcionistaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecepcionistaBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/recepcionista-backend/src/test/java/com/example/recepcionista_backend/RecepcionistaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recepcionista_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecepcionistaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
